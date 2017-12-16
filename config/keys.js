e47629a21e40e7b31d15ee48c23104708ca9edae
Ayman Idries <aymanov1993@gmail.com>
2017-12-16 11:35:40 +0200

    add

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: '622502260807-37b3vbqr1iq7v4l9a2u2ve4uup6hqdtv.apps.googleusercontent.com',
+  googleClientSecret: 'UkOSlSyzlEk1AtWQ0RP2VJPe',
+  mongoURI: 'mongodb://aymanov1993:ayman1993@ds159776.mlab.com:59776/emaily',
+  cookieKey: 'lkglfglfllgfdlgllglklfkdglkfdjkgjfdkgjfdg893432gkfdjgkfdjgldfglkdfjgkldfj'
+};
